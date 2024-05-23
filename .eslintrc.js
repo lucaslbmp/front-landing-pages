64bfe998a788a2bf62ce51f2e226ae3b467e1efd
Lucas <lucaslbmp@github.com>
2024-05-23 15:05:11 -0300

    Primeiro commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    "no-useless-catch": "off",
+    "no-unused-vars": "off"
+  },
+};
